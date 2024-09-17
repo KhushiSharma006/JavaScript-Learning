740e21860c19d57e302533688bcbe64165ec109d
Khushi Sharma <khushisharma.ks656@gmail.com>
2024-09-18 00:09:31 +0530

    done

--- 01.Basics/1.fundamentals.js
+++ 01.Basics/1.fundamentals.js
@@ -1,3 +1,4 @@
+
 "use strict" ; // treat all JS code as newer  version  
 
 console.log("Hey! What's up guys.")
--- 01.Basics/7.object.js
+++ 01.Basics/7.object.js
@@ -1,4 +1,6 @@
-///singltons ->Constructor method
+
+
+//singlton ->Constructor method
 //Object.create
 
 //Object Literals
@@ -22,18 +24,80 @@ const jsUser ={
 //console.log(jsUser.email);
 
 //method 2
-console.log(jsUser["email"]);
-console.log(jsUser["full name"]);
-console.log(jsUser[mySym]);
+//console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
 
 //to freeze the object
 //Object.freeze(jsUser)
 //jsUser.name = "legendKS"//this modification can't be done as object is freezed
 
+/*
+jsUser.greet = function(){
+    console.log("Hello JS user");
+}*/
 
 jsUser.greet = function(){
     console.log(`Hello JS user,${this.name}`);
 }
 
-console.log(jsUser.greet());
+//console.log(jsUser.greet());
+
+const regularUser ={
+    email : "abc@email.com",
+    fullname:{
+        name:"Khushi",
+        surname:"Vishwakarma"
+    }
+};
+
+//console.log(regularUser.fullname?.surname);
+ const obj1 ={1:"a", 2:"b"}
+ const obj2 ={3:"c", 4:"d"}
+
+ //const obj3 ={obj1 , obj2};
+ const obj3 = Object.assign({}, obj1, obj2);
+ //const obj3 = {...obj1 , ...obj2};
+
+ //console.log(obj3);
+
+ ///Array of object
+ const users = [
+    {
+        id:1,
+        email:"KS@gmail.com"
+    },
+    {
+        id:2,
+        email:"AS@gmail.com"
+    },
+    {
+        id:3,
+        email:"DS@gmail.com"
+    },
+    {
+        id:4,
+        email:"SS@gmail.com"
+    }
+ ]
+ //console.log(users[0].email);
+
+
+ //const tinderUser = new Object()
+ const tinderUser = {}
+
+ tinderUser.id = "legend006"
+ tinderUser.name = "qwerty"
+ tinderUser.isLoggedIn = true
+
+ //console.log(tinderUser);
+ //console.log(Object.keys(tinderUser)); //return in array
+ //console.log(Object.values(tinderUser));
+ //console.log(Object.entries(tinderUser));
+
+ //to check property exist or not
+ console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+
  
