544b366db5d196a5fbadf98217c663563a152c75
Khushi Sharma <khushisharma.ks656@gmail.com>
2024-09-17 10:35:23 +0530

    Array is done

--- 01.Basics/6.arrays.js
+++ 01.Basics/6.arrays.js
@@ -24,7 +24,7 @@ const newArr = arr.join() // convert array  into string
 //console.log(typeof newArr);
 
 // SLICE, SPLICE
-console.log(arr2);
+/*console.log(arr2);
 
 const myn1 = arr2.slice(1,3)
 console.log(myn1); // slice does not include outer limit and does not manipulate original arr 
@@ -32,4 +32,36 @@ console.log(myn1); // slice does not include outer limit and does not manipulate
 const myn2 = arr2.splice(1,3)
 console.log(myn2);  // splice include outer limit and manipulate original arr
 
-console.log(arr2);
+console.log(arr2);*/
+
+const mcu = ["Thor", "Black Panther", "Wanda", "Gamora"];
+const dc = ["Batman","Wonder Women", "Superman", "Flash", "Deadpool"]
+
+//mcu.push(dc)
+//console.log(mcu[4][1]);
+
+//concatination
+//const allHeroes = mcu.concat(dc)
+//console.log(allHeroes);
+
+//Spread
+// allHeroes = [...mcu, ...dc]
+//console.log(allHeroes);
+
+//FLAT: return a new array with all sub-array element concatinated into it recursively up to the specified depth
+const array = [1,2,3,[4,4,4],5,[6,7,[8,9]]];
+const flatArr = array.flat(Infinity);//you can give exact depth
+//console.log(flatArr);
+
+console.log(Array.isArray("JavaScript"));
+console.log(Array.from("JAVASCRIPT"));
+console.log(Array.from({name:"Khushi"}));//Interesting (obj is not converting into array)
+
+let s1 = 100
+let s2= 200
+let s3 = 300
+console.log(Array.of(s1,s2,s3));
+
+
+
+
