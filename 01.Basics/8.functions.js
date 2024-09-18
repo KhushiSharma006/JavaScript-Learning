b65d48d967e947e254e3b9db78555097a8f0955d
Khushi Sharma <khushisharma.ks656@gmail.com>
2024-09-18 11:18:04 +0530

    Basics of function

---
+++ 01.Basics/8.functions.js
@@ -0,0 +1,69 @@
+
+/*
+function greet(name){
+    console.log(`Welcome to JS learning journey${name}`);
+    //console.log(2+6);
+}
+console.log(greet("khushi"));
+*/
+
+// function add(num1,num2){
+//    // console.log(num1+num2);
+//     return num1+num2;
+// }
+// const result = add(5,6)
+// console.log("Result is:", result);
+
+/*
+ function message(username){
+    if(username === undefined){
+        console.log("Enter username");
+        return
+    }
+    else{
+        return `${username} Just Logged In`;
+    }
+ }
+ console.log(message("khushi"));
+ */
+
+
+// function newArr(n1, n2, ...n3){
+//     return n3;
+//  }
+//  console.log(newArr(10,20,30,40,50));
+
+
+/*
+//passing object in a function
+const user ={
+    name:"khushi",
+    id:"legendKS006"
+}
+//check the element you want to extact exist or not
+//the parameter you are passing is object or not (check type safety)
+function handleObj(anyobj){
+    console.log(`Username is ${anyobj.name} and id is ${anyobj.id}.`);
+}
+
+//handleObj(user)
+//OR
+handleObj({
+    name:"ayush",
+    id:"Hashira11"
+})
+    
+*/
+
+/*
+//passing array in a function
+const arr =[20,40,60,80]
+function handleaArr(getarr){
+    return getarr[1]
+}
+
+//console.log(handleaArr(arr));
+//Or
+console.log(handleaArr([5,15,25,35]));
+*/
+
