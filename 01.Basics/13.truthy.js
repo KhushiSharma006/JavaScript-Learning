3f8d56ac903a131d9f3a72230124b0f1fd637f44
Khushi Sharma <khushisharma.ks656@gmail.com>
2024-09-19 10:03:48 +0530

    truthy falsy values

--- 01.Basics/13.truthy.js
+++ 01.Basics/13.truthy.js
@@ -17,3 +17,20 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 }
+
+//Nullish Coalescing Operator (??) : null, undefined
+
+let val1 
+
+val1 = 5 ?? 10
+val1 = null??10
+val1 =undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+//console.log(val1)
+
+//Terniary Operation
+// condition ? true :false
+
+const age = 8
+age>=18 ? console.log("you can vote") : console.log("you can not vote")
