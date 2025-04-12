26a08089c57be655e1c57dcfffe9f8ce81d474b8
Khushi Sharma <khushisharma.ks656@gmail.com>
2025-04-13 00:41:01 +0530

    notes updates

--- 01.Basics/1.fundamentals.js
+++ 01.Basics/1.fundamentals.js
@@ -7,7 +7,7 @@ console.log("Hey! What's up guys.")
 
 const a = 5.5
 let b = "abc"
-var c = 464646 ;// dont use because of issue in block scope and functional scope  
+var c = 464646 ;// dont use because of issue in block scope and functional scope  (used in older version of JS)
 let d;    //typeof(d)=undefined
 let e = null; //typeof(e)=object
 
@@ -16,13 +16,13 @@ let e = null; //typeof(e)=object
 //---------------DATA TYPES------------
 
 /*
-Number : 2 to power 
-bigint
-string:" "
+Number (Range in memory address: 2 to power 53)
+bigint  
+string:" " or ' '
 boolean: true/false
-null: standalone value
+null: standalone value =>representation of null value (eg: temparature can not be 0 or undefined , therefor we use null)
 undefined:
-symbol :unique
+symbol :unique (will be used in react , figma etc)
 */
 
 //console.log(typeof(d))
@@ -33,7 +33,8 @@ symbol :unique
 let valInNum = Number(b)
 //console.log(typeof(valInNum));
 //console.log(valInNum); 
-/*
+
+/* 
   "545" => 545
   "545dfd" => NaN
   true <=>1
@@ -48,15 +49,27 @@ let valInNum = Number(b)
  let s1 = "Khu"
  let s2 = "shi"
  let s3 = s1+s2;
- //console.log(s3);
+ console.log(s3); 
  
- //console.log(1+"2")
- //console.log("1"+2)
- //console.log("1"+2+3)
- //console.log(1+2+"3")
+ console.log(1+"2")  
+  console.log("1"+2)
+ console.log("1"+2+3)
+ console.log(1+2+"3")
+
+ /*Output:
+ Khushi
+12
+12
+123
+33
+ */
+
+//Prefix and Postfix form mdn docx
+
 
  //---------Comparision-------
-////###Alert
+ /*AVOID THESE COMPARISION*/
+////###Alert (Comparision b/w different data types)
  //console.log(null > 0)
  //console.log(null == 0)
  //console.log(null >= 0)
@@ -66,7 +79,18 @@ let valInNum = Number(b)
    That's why null >= 0  is true and null > 0 is false. */
 
 
-   /// Strict Check
+   //console.log(undefined == 0);
+   //console.log(undefined > 0);
+   //console.log(undefined < 0);
+   //O/P : false
+
+
+   /// Strict Check (===)
+   // it checks value with the datatype
 
    console.log("2"==2);
-   console.log("2"===2);
+   console.log("2"===2); 
+
+   /* Output:
+   true
+   false */
